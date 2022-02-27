3cbe7adb8b4ab46a3aa2036a03974c7f89a4cb5a
Sebastian Sigl <sebarthel@eclassifiedsgroup.com>
2022-02-27 17:48:18 +0100

    Use kotlin grouping API

--- src/main/kotlin/org/example/AnagramDetector.kt
+++ src/main/kotlin/org/example/AnagramDetector.kt
@@ -3,30 +3,17 @@ package org.example
 class AnagramDetector {
     fun countAnagrams(words: List<String>): Int {
 
-        val sortedWordCounts = mutableMapOf<String, Int>()
-
-        words
-            .map(this::toSortedWord)
-            .forEach { sortedWord ->
-                incrementWordCount(sortedWordCounts, sortedWord)
-            }
+        val sortedWordCounts = countSortedWords(words)
 
         return sortedWordCounts.filterValues(this::moreThanOneExampleForSortedWord)
             .count()
     }
 
-    private fun moreThanOneExampleForSortedWord(value: Int) = value > 1
+    private fun countSortedWords(words: List<String>) = words
+        .groupingBy(this::toSortedWord)
+        .eachCount()
 
-    private fun incrementWordCount(
-        sortedWordCounts: MutableMap<String, Int>,
-        sortedWord: String
-    ) {
-        if (sortedWordCounts.contains(sortedWord).not()) {
-            sortedWordCounts[sortedWord] = 1
-        } else {
-            sortedWordCounts.compute(sortedWord) { _, v -> v!! + 1 }
-        }
-    }
+    private fun moreThanOneExampleForSortedWord(value: Int) = value > 1
 
     private fun toSortedWord(word: String): String {
         val arr = word.toCharArray()
@@ -41,28 +28,10 @@ class AnagramDetector {
             .map { entry -> entry.value }
     }
 
-    private fun withMoreThanOneExample(sortedWordList: MutableList<String>) =
+    private fun withMoreThanOneExample(sortedWordList: List<String>) =
         sortedWordList.count() > 1
 
-    private fun createSortedWordLists(words: List<String>): MutableMap<String, MutableList<String>> {
-        val sortedWordLists = mutableMapOf<String, MutableList<String>>()
-        words.forEach { word ->
-            val sortedWord = this.toSortedWord(word)
-            addWordToSortedWordList(sortedWordLists, sortedWord, word)
-        }
-        return sortedWordLists
-    }
-
-    private fun addWordToSortedWordList(
-        sortedWordLists: MutableMap<String, MutableList<String>>,
-        sortedWord: String,
-        word: String
-    ) {
-        if (sortedWordLists.contains(sortedWord).not()) {
-            sortedWordLists[sortedWord] = mutableListOf(word)
-        } else {
-            val sortedWordList = sortedWordLists.getValue(sortedWord)
-            sortedWordList.add(word)
-        }
+    private fun createSortedWordLists(words: List<String>): Map<String, List<String>> {
+        return words.groupBy(this::toSortedWord) { it }
     }
 }
